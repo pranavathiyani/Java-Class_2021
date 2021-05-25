831a7856933168ec73d5f1d05377615563c63345
Pranavathiyani G <pranavathiyani@users.noreply.github.com>
2021-05-25 14:27:17 +0530

    Added an A in input

--- Dna.java
+++ Dna.java
@@ -1,7 +1,7 @@
 class Dna{
 public static void main(String args[])
 {
-String dna="ATACTACTACTAC";
+String dna="ATACTACTACTACA";
 System.out.println("The DNA sequence is:"+dna+"\n and the length is "+ dna.length());
 }
 }
